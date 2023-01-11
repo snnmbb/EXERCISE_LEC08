77ae12bb4fe237894eb7977b39ef08ee6f472c64
Nunnalin Auephattanaphong <myiceskate1903@hotmail.com>
2023-01-11 14:32:15 +0700

    create file

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class ExceptionExample {
+    public static void main(String[] args) {
+
+        int a = 5, b = 0;
+
+        try {
+            int c = a / b;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
